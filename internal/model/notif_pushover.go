ff4874b6a80c41b7f0a4c7a32e9217e7f38e6b6c
CrazyMax <crazy-max@users.noreply.github.com>
2021-01-04 02:19:00 +0100

    Fix validation

--- internal/model/notif_pushover.go
+++ internal/model/notif_pushover.go
@@ -5,7 +5,7 @@ type NotifPushover struct {
 	Token         string `yaml:"token,omitempty" json:"token,omitempty" validate:"omitempty"`
 	TokenFile     string `yaml:"tokenFile,omitempty" json:"tokenFile,omitempty" validate:"omitempty,file"`
 	Recipient     string `yaml:"recipient,omitempty" json:"recipient,omitempty" validate:"omitempty"`
-	RecipientFile string `yaml:"recipientFile,omitempty" json:"recipientFile,omitempty" validate:"required"`
+	RecipientFile string `yaml:"recipientFile,omitempty" json:"recipientFile,omitempty" validate:"omitempty,file"`
 	Priority      int    `yaml:"priority,omitempty" json:"priority,omitempty" validate:"omitempty,min=-2,max=2"`
 }
 
