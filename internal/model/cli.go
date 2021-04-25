17f3ec05f42e3faf14e076e5921685e32c9729fb
CrazyMax <crazy-max@users.noreply.github.com>
2021-04-25 22:52:45 +0200

    Make profiler optional

--- internal/model/cli.go
+++ internal/model/cli.go
@@ -7,7 +7,7 @@ type Cli struct {
 	Version      kong.VersionFlag
 	Cfgfile      string `kong:"name='config',env='CONFIG',help='Diun configuration file.'"`
 	ProfilerPath string `kong:"name='profiler-path',env='PROFILER_PATH',help='Base path where profiling files are written.'"`
-	Profiler     string `kong:"name='profiler',env='PROFILER',enum='cpu,mem,alloc,heap,routines,mutex,threads,block',help='Profiler to use.'"`
+	Profiler     string `kong:"name='profiler',env='PROFILER',help='Profiler to use.'"`
 	LogLevel     string `kong:"name='log-level',env='LOG_LEVEL',default='info',help='Set log level.'"`
 	LogJSON      bool   `kong:"name='log-json',env='LOG_JSON',default='false',help='Enable JSON logging output.'"`
 	LogCaller    bool   `kong:"name='log-caller',env='LOG_CALLER',default='false',help='Add file:line of the caller to log output.'"`
