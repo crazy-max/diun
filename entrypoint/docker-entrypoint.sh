8f85872f9c13e808eeeb58cf6a13f16e1de06f8f
Id3aFly <Id3aFly@users.noreply.github.com>
2025-08-26 01:38:49 +0200

    Add shell script as new entrypoint

---
+++ entrypoint/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Exit immediately if a command fails
+set -o errexit
+
+# Treat unset variables as an error
+set -o nounset
+
+# If CA_CERT env var is set and points to an existing file
+if [ -n "$CA_CERTIFICATE" ] && [ -f "$CA_CERTIFICATE" ]; then
+    echo "Installing custom CA certificate from $CA_CERTIFICATE"
+    cp "$CA_CERTIFICATE" /usr/local/share/ca-certificates/custom-ca.crt
+    update-ca-certificates
+fi
+
+# Execute the command passed as arguments
+exec diun "$@"
