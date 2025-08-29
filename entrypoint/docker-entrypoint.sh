293966cbc710f39362b9f1dbbe03c2c0df021bb3
Id3aFly <Id3aFly@users.noreply.github.com>
2025-08-29 19:56:04 +0200

    Handle case with unset CA_CERTIFICATE variable correctly

--- entrypoint/docker-entrypoint.sh
+++ entrypoint/docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -o errexit
 set -o nounset
 
 # If CA_CERT env var is set and points to an existing file
-if [ -n "$CA_CERTIFICATE" ] && [ -f "$CA_CERTIFICATE" ]; then
+if [ -n "${CA_CERTIFICATE:-}" ] && [ -f "${CA_CERTIFICATE:-}" ]; then
     echo "Installing custom CA certificate from $CA_CERTIFICATE"
     cp "$CA_CERTIFICATE" /usr/local/share/ca-certificates/custom-ca.crt
     update-ca-certificates
