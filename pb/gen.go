8e520b8fc7915baed98db206228eb00a51eff356
JS Moore <js@jsmoore.dev>
2026-01-02 10:54:32 -0400

    Adding internal healthcheck command.
    Copying the notiftest pattern, create a healthcheck command that uses grpc to validate that Diun is running. Usage:
    
    docker compose exec diun diun healthcheck test
    
    Fixes #938.

--- pb/gen.go
+++ pb/gen.go
@@ -1,3 +1,3 @@
 package pb
 
-//go:generate protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. image.proto notif.proto
+//go:generate protoc --go_out=paths=source_relative:. --go-grpc_out=paths=source_relative:. image.proto notif.proto healthcheck.proto
